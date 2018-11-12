b56a0a70d73d842b33fa8806fe02d2582bfc1a22
arnaudcartailler <arnaud.cartailler@gmail.com>
2018-11-12 09:39:46 +0100

    add cards

--- js/main.js
+++ js/main.js
@@ -1,35 +1,49 @@
-
-//First method work only when refresh F5 but not with send button
-
-function Product(title, accro, summ) {
+function Cards(title, accro, summary) {
     this.title = title;
     this.accro = accro;
-    this.summ = summ;
+    this.summary = summary;
 }
 
-var productsinput = new Product(
-    document.getElementById("title").value, 
-    document.getElementById("accro").value, 
-    document.getElementById("summ").value
-    );
+function addCard() {
+    let titles = document.getElementById('title').value;
+    let accroches = document.getElementById('accro').value;
+    let summary = document.getElementById('summary').value;
+    newObject = new Cards(titles, accroches, summary);
+    document.getElementById('title').value = "";
+    document.getElementById('accro').value = "";
+    document.getElementById('summary').value = "";
 
-function send() {
+    let htmlManager = {
+        parentElement: document.getElementById('viewCard'),
+        card: [newObject],
+        createHTMLElement: function (element, title, accro, summary, parentElement) {
+            if (title !== "" && accro !== "" && summary !== "") {
+                let childElement = document.createElement(element);
+                childElement.className = "col-5 m-0 p-0 mx-auto mt-2";
+                parentElement.appendChild(childElement);
 
-document.getElementById("dtitle").innerHTML = productsinput.title;
-document.getElementById("daccro").innerHTML = productsinput.accro;
-document.getElementById("dsumm").innerHTML = productsinput.summ;
+                let titleElement = document.createElement(element);
+                titleElement.className = "col-12 m-0 p-0 text-center title";
+                titleElement.innerHTML = title;
+                childElement.appendChild(titleElement);
 
-function clear() {
+                let accrocheElement = document.createElement(element);
+                accrocheElement.className = "col-12 m-0 p-0 text-left accro";
+                accrocheElement.innerHTML = accro;
+                childElement.appendChild(accrocheElement);
 
-    document.getElementById("title").value = "";
-    document.getElementById("accro").value = "";
-    document.getElementById("summ").value = "";
+                let descriptionElement = document.createElement(element);
+                descriptionElement.className = "col-12 m-0 p-0 text-left desc";
+                descriptionElement.innerHTML = summary;
+                childElement.appendChild(descriptionElement);
+            }
+        },
+        displayCard: function (array) {
+            let that = this;
+            array.forEach(function (card) {
+                that.createHTMLElement('div', card.title, card.accro, card.summary, that.parentElement);
+            })
+        },
+    }
+    htmlManager.displayCard(htmlManager.card);
 }
-
-clear();
-
-}
-
-send();
-  
-
